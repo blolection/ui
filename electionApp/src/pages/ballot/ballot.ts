6b3d01233fde9e2453f57f8c0ef2c83851cfed7c
harishrewari@gmail.com <harishrewari@gmail.com>
2017-12-02 18:53:36 +0530

    Created Final Ballot Screen UI

--- electionApp/src/pages/ballot/ballot.ts
+++ electionApp/src/pages/ballot/ballot.ts
@@ -9,12 +9,16 @@ export class BallotPage {
 
   candidates:any;
   constructor(public navCtrl: NavController) {
-    this.candidates=[{name:"Vasundhara Raje", vote:false, img:"http://vasundhararaje.in/wp-content/uploads/2016/06/vasundhara-raje-vidyanjali-homepage.jpg"},{name:"Bhanu",vote: false,img:"https://timesofindia.indiatimes.com/photo/msid-58915663/58915663.jpg?143864"}];
+    this.candidates=[
+    {name:"Vasundhara Raje",  img:"http://vasundhararaje.in/wp-content/uploads/2016/06/vasundhara-raje-vidyanjali-homepage.jpg",symbol:"https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Lotos_flower_symbol.svg/150px-Lotos_flower_symbol.svg.png",description:"Blah, blah, Blah"},
+    {name:"Bhanu",img:"https://timesofindia.indiatimes.com/photo/msid-58915663/58915663.jpg?143864",description:"Tata tata tata"},
+    {name:"Manohar Lal", img:"",description:"Ola Gama beta tata"}];
   }
   
-  vote(name,vote){
+  vote(name){
 
-    console.log(`${name} got the vote: ${vote}`);
+    console.log(`${name} got the vote`);
+    
 
   }
 }
